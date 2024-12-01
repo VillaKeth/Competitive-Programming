2eedf88ad173b2af7c7a8f4f58f8539d358676c8
VillaKeth <villaketh@gmail.com>
2024-12-01 18:31:03 -0500

    Create Filling-Shapes.cpp

---
+++ 1000/Filling-Shapes.cpp
@@ -0,0 +1,45 @@
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#include <fstream> 
+#include <numeric>
+#define ll long long
+#define ld long double
+#define pb push_back
+#define cin std::cin
+#define cout std::cout
+#define endl '\n'
+#define vector std::vector
+#define pair std::pair
+#define hewwo cout << "hewwo" << endl;
+const ll MODDY = 1e9+7;
+const ll MODDY2 = 998244353;
+const ll MODDY3 = 1e8;
+
+// filling shapes
+int main()
+{
+    int n;
+    cin >> n;
+    if(n % 2 == 1)
+    {
+        cout << 0 << endl;
+    }
+    else
+    {
+        cout << (int)pow(2, n/2) << endl;
+    }
+    return 0;
+}
