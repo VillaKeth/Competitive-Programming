8243d786e72c97cf1f5b5bb0ce3e645983cb6442
VillaKeth <villaketh@gmail.com>
2025-01-07 17:15:30 -0500

    Create Brightness-Begins.cpp

---
+++ 1200/Brightness-Begins.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#include <fstream> 
+#include <numeric>
+#define ll long long
+#define ld long double
+#define pb push_back
+#define cin std::cin
+#define cout std::cout
+#define endl '\n'
+#define vector std::vector
+#define pair std::pair
+#define hewwo cout << "hewwo" << endl;
+const ll MODDY = 1000000007;
+const ll MODDY2 = 998244353;
+
+// brightness begins
+int main()
+{
+    unsigned ll t, k;
+    cin >> t;
+    while(t--)
+    {
+        cin  >> k;
+        ll originalS = sqrtl(k);
+        ll newS = sqrtl(k+originalS);
+        cout << k+newS << endl;
+    }
+    return 0;
+}
